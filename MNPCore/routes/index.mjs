4cb1431db7fe90946add0c6f2f6d27bd416d3705
exxidaus <kpinthawornrak@gmail.com>
2025-02-26 10:32:39 +0700

    feat: add facilities endpoint

--- MNPCore/routes/index.mjs
+++ MNPCore/routes/index.mjs
@@ -3,6 +3,7 @@ import usersRouter from './users.mjs'
 import reserveRouter from './reserve.mjs'
 import paymentRouter from './payment.mjs'
 import livehouseRouter from './livehouse.mjs'
+import facilitiesRouter from './facilities.mjs'
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use('/users', usersRouter);
 router.use('/livehouse', livehouseRouter);
 router.use('/reserve', reserveRouter);
 router.use('/payment', paymentRouter);
+router.use('/facilities', facilitiesRouter);
 
 export default router;
