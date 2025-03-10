18b3ef8654cd00c168182eff5d51558a68a3179f
KwnxpN <tonnum.kawin@gmail.com>
2025-03-10 15:10:48 +0700

    update

--- mnpproject/app/page.tsx
+++ mnpproject/app/page.tsx
@@ -4,10 +4,8 @@ import { useUser } from "@/contexts/UserContext";
 
 export default function Page() {
   const { user } = useUser();
-  if (!user) {
-    redirect("/main-landing");
-  }
-  if (user?.role === "manager") {
+  console.log("User data:", user);
+  if (user?.role == "manager") {
     redirect("/manager-landing");
   } else if (user?.role === "artist"){
     redirect("/main-landing");
